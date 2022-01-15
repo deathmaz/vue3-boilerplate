ddd162115a9b34fadd7db8ee1596918e7f7038ab
deathmaz <00maz1987@gmail.com>
2022-01-15 17:57:43 +0200

    wip

---
+++ vite-component-library/component/input.vue
@@ -0,0 +1,18 @@
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  props: {
+    title: {
+      type: String,
+    }
+  },
+});
+</script>
+
+<template>
+  <div>
+    <h3>{{ title }}</h3>
+    <input type="text" @input="$emit('input', $event)" />
+  </div>
+</template>
