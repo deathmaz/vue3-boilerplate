5291adecb0cf3b42dea2d7928af9fce01c913d84
deathmaz <00maz1987@gmail.com>
2022-01-15 23:41:38 +0200

    testing things

--- packages/vite-component-library/src/lib.ts
+++ packages/vite-component-library/src/lib.ts
@@ -1,2 +1,10 @@
-export { default as LibInput } from './components/input.vue';
-export { default as LibTest } from './components/test';
+import LibInput from './components/input.vue';
+import LibTest from './components/test';
+import LibHeadline from './components/headline.vue';
+
+export {
+  LibInput, LibTest, LibHeadline,
+};
+
+/* export { default as LibInput } from './components/input.vue';
+export { default as LibTest } from './components/test'; */
