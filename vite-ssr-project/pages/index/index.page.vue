fafce5a0ed16b85130645565573f0ba004f9d212
deathmaz <00maz1987@gmail.com>
2022-01-18 17:37:18 +0200

    added ssr project

---
+++ vite-ssr-project/pages/index/index.page.vue
@@ -0,0 +1,14 @@
+<template>
+  <h1>Welcome</h1>
+  <LibInput :title="'This input comes from design system'" />
+  This page is:
+  <ul>
+    <li>Rendered to HTML.</li>
+    <li>Interactive. <Counter /></li>
+  </ul>
+</template>
+
+<script lang="ts" setup>
+import Counter from './Counter.vue'
+import { LibInput } from 'vite-component-library';
+</script>
