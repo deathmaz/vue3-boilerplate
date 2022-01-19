f7a603c3c5a25d69fce5eb4e042f0c0e55ccc747
deathmaz <00maz1987@gmail.com>
2022-01-19 18:36:47 +0200

    refactoring

--- vite-ssr-project/pages/users/index.page.server.ts
+++ vite-ssr-project/pages/users/index.page.server.ts
@@ -13,5 +13,3 @@ export async function onBeforeRender(pageContext:PageContextBuiltIn) {
     }
   };
 }
-
-export const passToClient = ['pageProps', 'urlPathname'];
--- vite-ssr-project/pages/users/index.page.vue
+++ vite-ssr-project/pages/users/index.page.vue
@@ -1,11 +1,11 @@
 <template>
   <div>
-    <h1>{{user.name}}</h1>
+    <h1>{{ user.name }}</h1>
     <p>The height is: {{ user.height }}</p>
     <p>The mass is: {{ user.mass }}</p>
   </div>
 </template>
 
 <script setup lang="ts">
-  defineProps(['user'])
+defineProps(['user']);
 </script>
